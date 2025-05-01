124b65e5a0d9135b2f303ba5fb7fcfb233b68e9b
Fernando Bautista <fernando-bautista1@hotmail.com>
2025-05-01 16:12:07 -0600

    [WIP] Create products

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,18 @@
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsPositive, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @IsNotEmpty()
+    public name: string;
+
+    @IsNumber({
+        maxDecimalPlaces: 4,
+    })
+    @IsPositive()
+    @Min(0)
+    @Type(() => Number)
+    public price: number;
+
+}
